7a8a359f604834ccd12189ac4b70b833b19072b5
Alysson-Miguel <edicao.dash@gmail.com>
2023-11-13 20:33:40 -0300

    Correção da primeira seçao

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Helmet>
+    {/* <Helmet>
       <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
-    </Helmet>
+    </Helmet> */}
     <App />
   </React.StrictMode>
 );
