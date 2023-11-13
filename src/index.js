d4e4c5a06a6cf31f01d16ba7ee6bf29addae08c5
Alysson-Miguel <edicao.dash@gmail.com>
2023-11-13 20:38:22 -0300

    Alteração na primeira Seção

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Helmet from 'react-helmet';
-import favicon from '../src/Img/Logo.png'; // Substitua o caminho do ícone
-
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Helmet>
-      <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
-    </Helmet> */}
     <App />
   </React.StrictMode>
 );
